31da7fccfacd1c0abe1d90fe66e3743010ba0a85
Tony Duran <yoet92@gmail.com>
2020-01-16 14:39:37 -0300

    stable ver. 1.0.0.1

--- treetable.js
+++ treetable.js
@@ -1,27 +1,37 @@
 function TreeTable(idTable, settings) {
     'use strict';
 
-    if (!EsTipo('string', idTable) && !EsTipo('object', settings)) return false;
-
-    var columns = EsTipo('array', settings.columns) ? settings.columns : [];
-    var data = EsTipo('array', settings.data) ? settings.data : [];
-    var tree = EsTipo('array', settings.tree) ? settings.tree : [];
-
     var table = document.getElementById(idTable);
-    if (!table) return false;
+    if (!table || Object.prototype.toString.call(settings) !== "[object Object]") return false;
+
+    var allColumns = Array.isArray(settings.columns) ? settings.columns : [];
+    var data = Array.isArray(settings.data) ? settings.data : [];
+    var icons = Object.prototype.toString.call(icons) === "[object Object]" ? settings.icons : {};
+    var iconCollapsed = icons.collapsed ? icons.collapsed : 'fa fa-chevron-right';
+    var iconExpanded = icons.expanded ? icons.expanded : 'fa fa-chevron-down';
+    var columns = [];
+
+    for (var ac = 0, lenAc = allColumns.length; ac < lenAc; ac++) {
+        var currentColumn = allColumns[ac];
+        if (currentColumn['visible'] === false) continue;
+        columns.push(currentColumn);
+    }
 
     table.innerHTML = '';
 
     var thead = document.createElement('thead');
     var trHead = document.createElement('tr');
-    columns.forEach(function(c) {
+    var columnsLength = columns.length;
+
+    for (var cl = 0; cl < columnsLength; cl++) {
+        var ccl = columns[cl];
         var th = document.createElement('th');
-        var title = c.title ? c.title : '';
-        var className = c.className ? c.className : '';
+        var title = ccl.title ? ccl.title : '';
+        var className = ccl.className ? ccl.className : '';
         th.innerHTML = title;
         th.className = className;
         trHead.appendChild(th);
-    });
+    }
 
     thead.appendChild(trHead);
     table.appendChild(thead);
@@ -32,70 +42,87 @@ function TreeTable(idTable, settings) {
         var trEmpty = document.createElement('tr');
         var tdEmpty = document.createElement('td');
         tdEmpty.innerHTML = 'NO SE ENCONTRARON REGISTROS';
-        tdEmpty.colSpan = columns.length;
+        tdEmpty.colSpan = columnsLength;
         tdEmpty.style.textAlign = 'center';
         trEmpty.appendChild(tdEmpty);
         tbody.appendChild(trEmpty);
     }
     function actionLink() {
-        var $this = this;
+        var icon = this;
         var tr = this.parentNode.parentNode;
         var target = '[data-parent="' + tr.getAttribute('data-index') + '"]';
-        var isExpanded = $this.getAttribute('class') === 'fa fa-chevron-down';
+        var isExpanded = tr.getAttribute('data-control') === 'expanded';
         var body = tr.parentNode;
         var children = body.querySelectorAll(target);
 
         if (!isExpanded) {
+
             for (var c = 0, len = children.length; c < len; c++) {
                 var current = children[c];
-                current.setAttribute('class', 'collapse in');
+                current.style.display = '';
                 var a = current.querySelector('a[icon-tree-table]');
-                if (a) a.className = 'fa fa-chevron-right';
+                if (a) a.className = iconCollapsed;
             }
-            $this.className = 'fa fa-chevron-down';
+            icon.className = iconExpanded;
+            tr.setAttribute('data-control', 'expanded');
         } else {
-            var closeChildren = function(children) {
+
+            var closeChildren = function (children) {
                 for (var i = 0, len = children.length; i < len; i++) {
                     var current = children[i];
-                    current.setAttribute('class', 'collapse');
+                    current.style.display = 'none';
                     var id = current.getAttribute('data-index');
                     var child = body.querySelectorAll('[data-parent="' + id + '"]');
-                    var a = current.querySelector('a[class="fa fa-chevron-down"]');
-                    if (child.length > 0 && a) closeChildren(child);
+                    var currentExpanded = current.getAttribute('data-control') === 'expanded';
+                    if (child.length > 0 && currentExpanded) {
+                        current.setAttribute('data-control', 'collapsed');
+                        closeChildren(child);
+                    }
                 }
             };
             closeChildren(children);
-            $this.className = 'fa fa-chevron-right';
+            icon.className = iconCollapsed;
+            tr.setAttribute('data-control', 'collapsed');
         }
     }
 
     var index = -1;
     function level(dataLevel, lv, parentRow) {
+
         for (var id = 0, len = dataLevel.length; id < len; id++) {
+
             index++;
             var row = dataLevel[id];
+            var treeChildren = [];
             var tr = document.createElement('tr');
-            var numCol = columns.length;
 
-            for (var i = 0; i < numCol; i++) {
-                var data = row[columns[i].data] ? row[columns[i].data] : '';
-                var text = EsTipo('function', columns[i].render)
-                    ? columns[i].render(data, tr, row, index)
-                    : data;
+            for (var ch in row) {
+                if (Array.isArray(row[ch]) && row[ch].length > 0) treeChildren.push(ch);
+            }
+
+            for (var c = 0; c < columnsLength; c++) {
+                row[columns[c].data] = row[columns[c].data] ? row[columns[c].data] : '';
+            }
+
+            for (var i = 0; i < columnsLength; i++) {
+
+                var data = row[columns[i].data];
+                var text = typeof columns[i].render === 'function' ? columns[i].render(data, tr, row, index) : data;
                 var content = document.createElement('div');
                 content.style.display = 'inline-block';
                 content.innerHTML = text;
-                if (i === 0 && EsTipo('array', row[tree[lv - 1]]) && row[tree[lv - 1]].length > 0) {
+                if (i === 0 && treeChildren.length > 0) {
                     var tdPrimary = document.createElement('td');
                     var icon = document.createElement('a');
-                    icon.className = 'fa fa-chevron-right';
+                    icon.className = iconCollapsed;
                     icon.setAttribute('icon-tree-table', true);
                     icon.addEventListener('click', actionLink, false);
                     tdPrimary.appendChild(icon);
                     tdPrimary.appendChild(content);
                     tdPrimary.className = columns[i].className ? columns[i].className : '';
-                    tdPrimary.setAttribute('style', 'padding-left: ' + lv + 'em !important');
+                    tdPrimary.setAttribute('style', 'padding-left: ' + lv * 11 + 'px !important');
                     tr.appendChild(tdPrimary);
+                    tr.setAttribute('data-control', 'collapsed');
                 } else {
                     var td = document.createElement('td');
                     td.appendChild(content);
@@ -103,21 +130,19 @@ function TreeTable(idTable, settings) {
                     tr.appendChild(td);
                 }
                 if (lv > 1) {
-                    tr.className = 'collapse';
+                    tr.style.display = 'none';
                     tr.setAttribute('data-parent', parentRow);
                 }
                 tr.setAttribute('data-index', index);
+                tr.setAttribute('data-level', lv);
             }
             tbody.appendChild(tr);
 
-            for (var t = 0, lent = tree.length; t < lent; t++) {
-                var ct = tree[lv - 1];
+            for (var t = 0, lent = treeChildren.length; t < lent; t++) {
 
-                if (EsTipo('array', row[ct])) {
-                    level(row[ct], lv + 1, index);
-                } else {
-                    break;
-                }
+                var ct = treeChildren[t];
+
+                level(row[ct], lv + 1, index);
             }
         }
     }
